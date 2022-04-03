e4e5f95f7d81f4bf07e1978517b843573a429bae
Synui <jonathon.schoenholz@yahoo.con>
2022-04-03 12:33:10 -0400

    typeDefs added

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware
+    // context: authMiddleware
   });
 
   // Start the Apollo server
@@ -43,9 +43,9 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
